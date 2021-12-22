bd3b583da93c6724c6d71eb6663c2cba79b427ca
João Pedro <jpedrodaibello1999@outlook.com>
2021-12-22 11:02:37 -0300

    feat: add configuring MySQL database

--- lib/application/config/application_config.dart
+++ lib/application/config/application_config.dart
@@ -1,12 +1,25 @@
+import 'package:cuidapet_api/application/config/database_connection_configuration.dart';
 import 'package:dotenv/dotenv.dart' show load, env;
+import 'package:get_it/get_it.dart';
 
 class ApplicationConfig {
   Future<void> loadConfigApplication() async {
     await _loadEnv();
-    final variavel = env['DATABASE_URL'];
-
-    print(variavel);
+    _loadDatabaseConfig();
   }
 
   Future<void> _loadEnv() async => load();
+
+  void _loadDatabaseConfig() {
+    final databaseConfig = DatabaseConnectionConfiguration(
+      host: env['DATABASE_HOST'] ?? env['LOCAL_DATABASE_HOST']!,
+      user: env['DATABASE_USER'] ?? env['LOCAL_DATABASE_USER']!,
+      port: int.tryParse(env['DATABASE_PORT'] ?? env['LOCAL_DATABASE_PORT']!) ??
+          0,
+      password: env['DATABASE_PASSWORD'] ?? env['DATABASE_PASSWORD']!,
+      databaseName: env['DATABASE_NAME'] ?? env['DATABASE_NAME']!,
+    );
+
+    GetIt.I.registerSingleton(databaseConfig);
+  }
 }
