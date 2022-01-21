1e383ae8f6ea79a9262e7fa14c4c2b519acd9d32
João Pedro <jpedrodaibello1999@outlook.com>
2022-01-20 22:11:12 -0300

    test: add improving MySql mocks architecture

--- test/core/mysql/mock_results.dart
+++ test/core/mysql/mock_results.dart
@@ -1,4 +1,37 @@
+import 'dart:convert';
+
 import 'package:mocktail/mocktail.dart';
 import 'package:mysql1/mysql1.dart';
 
-class MockResults extends Mock implements Results {}
+import 'mock_result_row.dart';
+
+class MockResults extends Mock implements Results {
+  final List<ResultRow> _rows = [];
+
+  @override
+  Iterator<ResultRow> get iterator => _rows.iterator;
+
+  @override
+  bool get isEmpty => !iterator.moveNext();
+
+  @override
+  ResultRow get first {
+    var it = iterator;
+    if (!it.moveNext()) {
+      throw Exception();
+    }
+
+    return it.current;
+  }
+
+  MockResults([String? json, List<String>? blobFields]) {
+    if (json != null) {
+      final data = jsonDecode(json);
+      if (data is List) {
+        _rows.addAll(data.map((f) => MockResultRow(f, blobFields)).toList());
+      } else {
+        _rows.add(MockResultRow(data, blobFields));
+      }
+    }
+  }
+}
--- test/modules/user/data/user_repository_test.dart
+++ test/modules/user/data/user_repository_test.dart
@@ -1,47 +1,53 @@
-import 'package:cuidapet_api/application/database/i_database_connection.dart';
+import 'dart:convert';
+
+import 'package:cuidapet_api/application/exceptions/user_not_found_exception.dart';
 import 'package:cuidapet_api/application/logger/i_logger.dart';
 import 'package:cuidapet_api/entities/user.dart';
 import 'package:cuidapet_api/modules/user/data/user_repository.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:mysql1/mysql1.dart';
 import 'package:test/test.dart';
 
+import '../../../core/fixture/fixture_reader.dart';
 import '../../../core/log/mock_logger.dart';
-import '../../../core/mysql/mock_database_connection.dart';
-import '../../../core/mysql/mock_mysql_connection.dart';
-import '../../../core/mysql/mock_result_row.dart';
-import '../../../core/mysql/mock_results.dart';
+import '../../../core/mysql/mysql_mocks.dart';
 
 void main() {
-  late IDatabaseConnection database;
+  late MockDatabaseConnection database;
   late ILogger log;
-  late MySqlConnection mySqlConnection;
-  late Results mySqlResults;
-  late MockResultRow mySqlResultRow;
 
   setUp(() {
     database = MockDatabaseConnection();
     log = MockLogger();
-    mySqlConnection = MockMysqlConnection();
-    mySqlResults = MockResults();
-    mySqlResultRow = MockResultRow();
   });
 
   group('Group test findById', () {
     test('should return user by id', () async {
       //Arrange
       final userId = 1;
+      final userFixtureDB = FixtureReader.getJsonData(
+        'modules/user/data/fixture/find_by_id_success_fixture.json',
+      );
+
+      final mockResults = MockResults(userFixtureDB, [
+        'ios_token',
+        'android_token',
+        'refresh_token',
+        'img_avatar',
+      ]);
+
       final userRepository = UserRepository(connection: database, log: log);
 
-      when(() => mySqlConnection.close()).thenAnswer((_) async => _);
-      when(() => mySqlResults.first).thenReturn(mySqlResultRow);
-      when(() => mySqlResultRow['id']).thenAnswer((_) => 1);
-      when(() => mySqlConnection.query(any(), any())).thenAnswer(
-        (_) async => mySqlResults,
-      );
-      when(() => mySqlResults.isEmpty).thenReturn(false);
-      when(() => database.openConnection()).thenAnswer(
-        (_) async => mySqlConnection,
+      database.mockQuery(mockResults);
+
+      final usersMap = jsonDecode(userFixtureDB);
+      final userExpected = User(
+        id: usersMap['id'],
+        email: usersMap['email'],
+        registerType: usersMap['tipo_cadastro'],
+        iosToken: usersMap['ios_token'],
+        androidToken: usersMap['android_token'],
+        refreshToken: usersMap['refresh_token'],
+        imageAvatar: usersMap['img_avatar'],
+        supplierId: usersMap['fornecedor_id'],
       );
 
       //Act
@@ -49,7 +55,45 @@ void main() {
 
       //Assert
       expect(user, isA<User>());
-      expect(user.id, 1);
+      expect(user, userExpected);
+      database.verifyConnectionClose();
+    });
+
+    test('should return exception UserNotFoundException option 1', () async {
+      //Arrange
+      final id = 1;
+      final mockResults = MockResults();
+      database.mockQuery(mockResults, [id]);
+      final userRepository = UserRepository(connection: database, log: log);
+
+      //Act
+      var call = userRepository.findById;
+
+      //Assert
+      expect(() => call(id), throwsA(isA<UserNotFoundException>()));
+      await Future.delayed(Duration(seconds: 1));
+      database.verifyConnectionClose();
+    });
+
+    test('should return exception UserNotFoundException option 2', () async {
+      //Arrange
+      final id = 1;
+      final mockResults = MockResults();
+      database.mockQuery(mockResults, [id]);
+      final userRepository = UserRepository(connection: database, log: log);
+
+      //Act
+      try {
+        await userRepository.findById(id);
+      } catch (e) {
+        if (e is UserNotFoundException) {
+        } else {
+          fail('Exception errada, deveria retornar um UserNotFoundException');
+        }
+      }
+
+      //Assert
+      database.verifyConnectionClose();
     });
   });
 }
